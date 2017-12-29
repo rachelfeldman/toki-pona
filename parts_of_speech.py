6ca867873b08313f1c58287a9b995ef9c42c437f
Rachel Feldman <rachel.a.f.@Rachels-MacBook-Pro.local>
2017-12-29 13:02:24 -0800

    adds the parts of speech dictionary

---
+++ parts_of_speech.py
@@ -0,0 +1,13 @@
+parts_of_speech = {
+ 'n': 'noun',
+ 'mod': 'modifier',
+ 'sep': 'separator',
+ 'vi': 'intransitive verb',
+ 'vt': 'transitive verb',
+ 'interj': 'interjection',
+ 'prep': 'preposition',
+ 'conj': 'conjunction',
+ 'kama': 'compound verb preceded by kama',
+ 'cont': 'context word used before la',
+ 'oth': 'special other word'
+}
